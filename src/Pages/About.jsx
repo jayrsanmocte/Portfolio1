ed1ffcb27c0c5e3501406519c8c21a54f1badfa5
jayrsanmocte <133185464+jayrsanmocte@users.noreply.github.com>
2023-08-14 01:16:08 +0800

    first commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,38 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const About = () => {
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.querySelector('.about');
+      if (element) {
+        const elementPosition = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Add or remove the animation class based on scroll position
+        if (elementPosition - windowHeight <= 0) {
+          setShouldAnimate(true);
+        } else {
+          setShouldAnimate(false);
+        }
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className=" container about">
-      <h2>About Me</h2>
-      <p>
-        My name is Jay-r San Mocte, an aspiring Frontend Developer with a strong
-        foundation in React.js, JavaScript, HTML, and CSS. I am seeking
-        opportunities to leverage my technical expertise and creative skills to
-        build immersive and user-friendly web applications.
-      </p>
-      <h3>Languages & Frameworks</h3>
-      <p>
-        As an aspiring Frontend Developer, I have a strong foundation in
-        React.js, JavaScript, HTML, and CSS. I am dedicated to honing my skills
-        and contributing to the development of engaging web experiences.
-      </p>
-      <h3>Skills</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Bootstrap</li>
-        <li>React JS</li>
-        <li>Laravel</li>
-      </ul>
-      <h3>Database, Tools & Other Skills</h3>
-      <ul>
-        <li>Vercel</li>
-        <li>MySQL</li>
-        <li>phpMyAdmin</li>
-        <li>Git/Git Bash</li>
-        <li>Photoshop</li>
-      </ul>
+    <div className={`container about ${shouldAnimate ? 'animate__animated animate__fadeInRight' : ''}`}>
+        <div className="row">
+        <div className="container row align-items-center">
+        <h2 className="">About Me</h2>
+        <div className="line"></div> {/* Horizontal line */}
+      </div>
+                           
+                            
+                            <div className="col">
+                            <p>
+                            My name is Jay-r San Mocte, an aspiring Frontend Developer with a strong
+                            foundation in React.js, JavaScript, HTML, and CSS. I am seeking
+                            opportunities to leverage my technical expertise and creative skills to
+                            build immersive and user-friendly web applications.
+                        </p>
+                        
+                        
+                        
+                        <h3>Languages & Frameworks</h3>
+                        <p>
+                            As an aspiring Frontend Developer, I have a strong foundation in
+                            React.js, JavaScript, HTML, and CSS. I am dedicated to honing my skills
+                            and contributing to the development of engaging web experiences.
+                        </p>
+                        <h3>Skills</h3>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Bootstrap</li>
+                            <li>React JS</li>
+                            <li>Laravel</li>
+                        </ul>
+                        <h3>Database, Tools & Other Skills</h3>
+                        <ul>
+                            <li>Vercel</li>
+                            <li>MySQL</li>
+                            <li>phpMyAdmin</li>
+                            <li>Git/Git Bash</li>
+                            <li>Photoshop</li>
+                        </ul>
+                </div>
+                 
+                <div className="col ">
+                        <img src="/Images/me.avif" className="imageabout" alt="" />
+                    </div>
+        </div>
+      
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,36 +2,45 @@ import React from 'react';
 import About from './About';
 
 const Home = () => {
+  const handleContactButtonClick = () => {
+    window.open(
+      'mailto:jayrsanmocte08@gmail.com?subject=Let%27s%20Talk&body=Hello%20Jay-r,%0D%0A%0D%0AI%20would%20like%20to%20discuss%20...',
+      '_blank'
+    );
+  };
+  
   return (
     <>
-    <div className="content">
-      <div className="container animate__animated animate__fadeInUp animate__delay-1s ">
-        <div className="row">
-          <div className="col-md-6">
-            <h1 className="hero-text">Front-End React</h1>
-            <h1 className="hero-text1">
-              Developer{' '}
-              <img
-                className="hands animate__animated animate__tada animate__infinite"
-                src="https://stefantopalovicdev.vercel.app/static/media/waving.1bae5fcfb51082b5c2b4.png"
-                alt="waving_hand"
-              />
-            </h1>
-            <p className="description">Hi, I'm Jay-r San Mocte</p>{' '}
-            <p className="description2">
-              A passionate Front End React Developer Based in Philippines
-            </p>
-            <button className="contact-button">Let's Talk</button>
-          </div>
-          <div className="col-md-6">
-            <div className="hero-img"></div>
+      <div className="content">
+        <div className="container animate__animated animate__fadeInUp animate__delay-1s ">
+          <div className="row">
+            <div className="col-md-6">
+              <h1 className="hero-text">Front-End React</h1>
+              <h1 className="hero-text1">
+                Developer{' '}
+                <img
+                  className="hands animate__animated animate__tada animate__infinite"
+                  src="https://stefantopalovicdev.vercel.app/static/media/waving.1bae5fcfb51082b5c2b4.png"
+                  alt="waving_hand"
+                />
+              </h1>
+              <p className="description">Hi my name is, </p>{' '}
+              <p className='hero-text3'> Jay-r San Mocte</p>
+              <p className="description2">
+                A passionate Front End React Developer Based in Philippines
+              </p>
+              <button className="contact-button" onClick={handleContactButtonClick}>
+                Let's Talk
+              </button>
+            </div>
+            <div className="col-md-6">
+              <div className="hero-img"></div>
+            </div>
           </div>
         </div>
-       
-      </div>
       </div>
       <About />
-</>    
+    </>
   );
 };
 
