a14e7163b3d5e26d24e7545800bc042925204efa
jayrsanmocte <133185464+jayrsanmocte@users.noreply.github.com>
2023-08-13 22:03:45 +0800

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,37 +1,38 @@
-  import React from 'react'
-import About from './About'
- 
-  
-  const Home = () => {
-    return (
-      <>
-      <div className="content">
-              <div className="container ">
-                  <div className="row">
-                    <div className="col">
-                            <h1 className="hero-text">Front-End React</h1>    
-                          <h1 className="hero-text1 ">Developer  <img className="hands animate__animated animate__tada animate__infinite 	"src="https://stefantopalovicdev.vercel.app/static/media/waving.1bae5fcfb51082b5c2b4.png" alt="waving_hand"></img> </h1> 
-                          <p className="description">Hi i’m Jay-r San Mocte </p> <p className="description2"> A pasionate Front End React Developer Based in Philippines</p>
-                          <button className="contact-button">Let's Talk</button>
-                        
-                    </div>
-                    <div className="col">
-                    <div className="hero-img"></div>
-                    </div>
-                    
-                  </div>
-                  <div className="row">
-                  <p>Tech Stack</p>
-                  <img src="https://stefantopalovicdev.vercel.app/static/media/html.6e7b1b463015c056aeb9a624c8dc2876.svg" alt="" className='skill' />
-                  <img src="https://stefantopalovicdev.vercel.app/static/media/css3.9cecabbf6ce67609c48bc4f280a11002.svg" alt=""className='skill' />
-                  <img src="https://stefantopalovicdev.vercel.app/static/media/react.afac9c43724070bf6674f2692b7356a9.svg" alt="" className='skill' />
-                  </div>
-              
-            </div>
-      </div>  
-        <About />
-        </>
-    )
-  }
+import React from 'react';
+import About from './About';
 
-  export default Home
+const Home = () => {
+  return (
+    <>
+    <div className="content">
+      <div className="container animate__animated animate__fadeInUp animate__delay-1s ">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="hero-text">Front-End React</h1>
+            <h1 className="hero-text1">
+              Developer{' '}
+              <img
+                className="hands animate__animated animate__tada animate__infinite"
+                src="https://stefantopalovicdev.vercel.app/static/media/waving.1bae5fcfb51082b5c2b4.png"
+                alt="waving_hand"
+              />
+            </h1>
+            <p className="description">Hi, I'm Jay-r San Mocte</p>{' '}
+            <p className="description2">
+              A passionate Front End React Developer Based in Philippines
+            </p>
+            <button className="contact-button">Let's Talk</button>
+          </div>
+          <div className="col-md-6">
+            <div className="hero-img"></div>
+          </div>
+        </div>
+       
+      </div>
+      </div>
+      <About />
+</>    
+  );
+};
+
+export default Home;
