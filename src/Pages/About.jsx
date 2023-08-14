10a9683156d95acddd749e6811b7fcf5cfa59b5c
jayrsanmocte <133185464+jayrsanmocte@users.noreply.github.com>
2023-08-14 18:02:53 +0800

    first commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,59 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
 const About = () => {
-  const [shouldAnimate, setShouldAnimate] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const element = document.querySelector('.about');
-      if (element) {
-        const elementPosition = element.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        // Add or remove the animation class based on scroll position
-        if (elementPosition - windowHeight <= 0) {
-          setShouldAnimate(true);
-        } else {
-          setShouldAnimate(false);
-        }
-      }
-    };
-
-    // Attach the scroll event listener
-    window.addEventListener('scroll', handleScroll);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+ 
 
   return (
-    <div className={`container about ${shouldAnimate ? 'animate__animated animate__fadeInRight' : ''}`}>
+    <div data-aos="fade-down">
+    <div className="container about colorwhite">
+     
         <div className="row">
-        <div className="container row align-items-center">
-        <h2 className="">About Me</h2>
-        <div className="line"></div> {/* Horizontal line */}
+        <div className="container row align-items-center ">
+       <span className='d-flex align-items-center'><h2 className="colorgreen"> About Me </h2>
+        <div className="line"></div></span>
       </div>
                            
                             
                             <div className="col">
                             <p>
-                            My name is Jay-r San Mocte, an aspiring Frontend Developer with a strong
-                            foundation in React.js, JavaScript, HTML, and CSS. I am seeking
+                            My name is <span style={{ color: '#64fedb' }}>Jay-r San Mocte</span>, an aspiring Frontend Developer with a strong
+                            foundation in Web Design, I am seeking
                             opportunities to leverage my technical expertise and creative skills to
                             build immersive and user-friendly web applications.
-                        </p>
+                          </p>
                         
                         
                         
-                        <h3>Languages & Frameworks</h3>
+                        <h3 className='colorgreen'>Languages & Frameworks</h3>
                         <p>
-                            As an aspiring Frontend Developer, I have a strong foundation in
-                            React.js, JavaScript, HTML, and CSS. I am dedicated to honing my skills
+                            As an aspiring Frontend Developer, I have a strong foundation in <span style={{ color: '#64fedb' }}>
+                            React.js, JavaScript, HTML, and CSS </span>. I am dedicated to honing my skills
                             and contributing to the development of engaging web experiences.
                         </p>
-                        <h3>Skills</h3>
+                        <h3 className='colorgreen'>Skills</h3>
+                        <div data-aos="fade-left">
                         <ul>
                             <li>HTML</li>
                             <li>CSS</li>
@@ -62,7 +40,9 @@ const About = () => {
                             <li>React JS</li>
                             <li>Laravel</li>
                         </ul>
-                        <h3>Database, Tools & Other Skills</h3>
+                        </div>
+                        <h3 className='colorgreen'>Database, Tools & Other Skills</h3>
+                        <div data-aos="fade-left">
                         <ul>
                             <li>Vercel</li>
                             <li>MySQL</li>
@@ -70,6 +50,7 @@ const About = () => {
                             <li>Git/Git Bash</li>
                             <li>Photoshop</li>
                         </ul>
+                        </div>
                 </div>
                  
                 <div className="col ">
@@ -77,6 +58,7 @@ const About = () => {
                     </div>
         </div>
       
+    </div>
     </div>
   );
 };
--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,51 +2,19 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-center text-white" style={{ backgroundColor: '#f1f1f1' }}>
-      {/* Grid container */}
-      <div className="container pt-4">
-        {/* Section: Social media */}
-        <section className="mb-4">
-          {/* Facebook */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          
-          {/* Twitter */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-twitter"></i>
-          </a>
-
-          {/* Google */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-google"></i>
-          </a>
-
-          {/* Instagram */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-instagram"></i>
-          </a>
-
-          {/* Linkedin */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          
-          {/* Github */}
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">
-            <i className="fab fa-github"></i>
-          </a>
-        </section>
-        {/* Section: Social media */}
+    <footer>
+      <a href="https://www.facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-facebook" alt="Facebook" ></i>
+        </a>
+        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-github"></i>
+        </a>
+        <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-instagram" alt="Instagram" ></i>
+        </a>
+      <div className="footer-right">
+        {/* Right footer content */}
       </div>
-      {/* Grid container */}
-
-      {/* Copyright */}
-      <div className="text-center text-dark p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-      </div>
-      {/* Copyright */}
     </footer>
   );
 };
