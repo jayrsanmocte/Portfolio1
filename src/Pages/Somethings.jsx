6e206b0d23091ebda4a7704a7e075dc880a6844a
jayrsanmocte <133185464+jayrsanmocte@users.noreply.github.com>
2023-08-16 11:58:43 +0800

    first commit3

--- src/Pages/Somethings.jsx
+++ src/Pages/Somethings.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const Somethings = () => {
   return (
+    <>
     <div data-aos="fade-down">
     
     <div className="container mt-5">
@@ -30,9 +31,9 @@ const Somethings = () => {
             <div className="col ">
                 
                 <a href="https://github.com/jayrsanmocte/doggy1" target="_blank" rel="noopener noreferrer" >
-                  <i className=" fab fa-github fa-3x " data-aos="flip-right"></i></a> &nbsp;  &nbsp; &nbsp; 
+                  <i className=" fab fa-github fa-3x " ></i></a> &nbsp;  &nbsp; &nbsp; 
                   <a href="https://doggy-doggy-fjiwpb9h3-mcdha.vercel.app/" target="_blank" rel="noopener noreferrer">
-              <i class="fab fas fa-up-right-from-square fa-3x" data-aos="flip-right"></i>
+              <i class="fab fas fa-up-right-from-square fa-3x" ></i>
               </a>  
             </div>
           
@@ -47,6 +48,52 @@ const Somethings = () => {
     </div>
     </div>   
      
+     
+     <div data-aos="fade-down">
+    
+     <div className="container padder">
+       <span className='d-flex align-items-center'>
+        
+         {/* <div className="line3"></div> */}
+       </span>
+       
+       <div className="row">
+         
+         <div className="card " data-aos="zoom-in">
+           <div className="scrollable-image">
+             <div className="image-overlay"></div>
+             <img className="card-img-top" src="/Images/plumbing.png" alt="Title" />
+           </div>
+         </div>
+         <div className="col md-12 sm-6">
+           <ul>
+             <div className="description" data-aos="zoom-in-left">Project</div>
+             <li className='description2' data-aos="zoom-in-left">Plumbing Service Website</li>
+           </ul>
+          
+          <div className="line2 md-12"> <h5 className='description3 text-center pt-3' data-aos="fade-left">Our plumbing service, powered by the dynamic duo of <span style={{ color: '#64fedb' }}>React JS and Bootstrap</span>. Discover a world of efficient plumbing solutions tailored to your needs, much like selecting the perfect companion for your home:</h5> </div>
+            <div className="row">
+             <div className="col ">
+                 
+                 <a href="https://github.com/jayrsanmocte/doggy1" target="_blank" rel="noopener noreferrer" >
+                   <i className=" fab fa-github fa-3x " ></i></a> &nbsp;  &nbsp; &nbsp; 
+                   <a href="https://doggy-doggy-fjiwpb9h3-mcdha.vercel.app/" target="_blank" rel="noopener noreferrer">
+               <i class="fab fas fa-up-right-from-square fa-3x" ></i>
+               </a>  
+             </div>
+           
+              
+               
+               
+            </div>
+           
+         </div>
+         
+       </div>
+     </div>
+     </div>   
+  </>
+
   );
 }
 
